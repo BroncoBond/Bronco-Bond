625c0612ea9601571dc0bdb27d7885dc2a0e9af0
Vivian Trieu <vivian.dtrieu@gmail.com>
2023-11-12 23:59:43 -0800

    reorganized code

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -1,5 +1,6 @@
-import 'package:bronco_bond/src/screens/signin.dart';
+import 'package:bronco_bond/src/screens/userInfo.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 /// Displays detailed information about a SampleItem.
 class LoginPage extends StatelessWidget {
@@ -8,26 +9,52 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page',
-          style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.onSecondary,
-                )),
-        backgroundColor: Theme.of(context).colorScheme.secondary,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('This is another page.'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SigninPage()),
-                  );
-              },
-              child: const Text('Go to Sign Up Page'),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Stack(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  "BroncoBond",
+                  style: GoogleFonts.raleway(
+                    textStyle: Theme.of(context).textTheme.displaySmall,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w800,
+                    color: const Color(0xFF3B5F43),
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const UserInfoPage(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Create Account",
+                    style: GoogleFonts.raleway(
+                      textStyle: Theme.of(context).textTheme.displaySmall,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: const Color(0xFF3B5F43),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
