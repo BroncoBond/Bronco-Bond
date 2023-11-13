2b1e777faf8af4ae776f5cf2408ee95447549e34
Vivian Trieu <vivian.dtrieu@gmail.com>
2023-11-12 23:59:18 -0800

    added comments

--- lib/src/screens/signin.dart
+++ lib/src/screens/signin.dart
@@ -8,84 +8,76 @@ class SigninPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color.fromARGB(225, 18, 32, 47),
-      ),
-      home: const Scaffold(
-        body: SignInView(),
-      ),
-    );
-  }
-}
-
-class SignInView extends StatelessWidget {
-  const SignInView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: const BoxDecoration(
-        color: Color(0xFF3B5F43),
-      ),
-      child: Stack(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 160.0),
-                child: Image.asset('assets/images/BroncoBond_logo.png'),
-              ),
-            ],
-          ),
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Image.asset(
-                'assets/images/bg_asset.png',
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.cover,
-              ),
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          color: Color(0xFF3B5F43),
+        ),
+        child: Stack(
+          children: [
+            // Logo image
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 160.0),
+                  child: Image.asset('assets/images/BroncoBond_logo.png'),
+                ),
+              ],
             ),
-          ),
-          Positioned(
-            bottom: 12.0,
-            right: 12.0,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginPage()),
-                  );
-                },
-                child: Row (
-                  children: [
-                    Text(
-                      "Join Now",
-                      style: GoogleFonts.raleway(
-                        textStyle: Theme.of(context).textTheme.displaySmall,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(),
-                        Image.asset(
-                          'assets/images/arrow_right.png',
-                          width: 25.0,
-                          height: 25.0,
-                        ),
-                  ],
+            // Background image, ordered behind the button
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset(
+                  'assets/images/bg_asset.png',
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
-          ),
-        ],
+            // Join Now button
+            Positioned(
+              bottom: 12.0,
+              right: 12.0,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                  // Button functionality
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()),
+                    );
+                  },
+                  // Button text and icon
+                  child: Row(
+                    children: [
+                      Text(
+                        "Join Now",
+                        style: GoogleFonts.raleway(
+                          textStyle: Theme.of(context).textTheme.displaySmall,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(),
+                      Image.asset(
+                        'assets/images/arrow_right.png',
+                        width: 25.0,
+                        height: 25.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
