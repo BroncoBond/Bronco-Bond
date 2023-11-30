a8f1ac76619b3c33e5221a554a010dbe339561c2
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2023-11-29 21:38:20 -0800

    Adding Nodejs

--- routers/user.router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const UserController = require("../controller/user.controller");
-
-router.post('/registration',UserController.register);
-router.post('/login',UserController.login)
-
-module.exports = router;
