23cbcf54f92cdfd54a5ae60594e9fd7146545688
Vivian Trieu <vivian.dtrieu@gmail.com>
2023-12-03 20:55:54 -0800

    added mia files

--- lib/src/screens/interests.dart
+++ lib/src/screens/interests.dart
@@ -31,136 +31,201 @@ class InterestsPage extends StatelessWidget {
           ),
           centerTitle: true,
         ),
-        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            Text("    How do you want to be involved?"),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(
+        body: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                Text("    How do you want to be involved?"),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SearchPage(),
+                          ));
+                    },
+                    child: Text(
+                      'Skip',
+                      style: GoogleFonts.raleway(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                      ),
+                    ))
+              ]),
+              Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: buildSearchBar(" ")),
+              Text(
+                "Educational",
+                style: GoogleFonts.raleway(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                buildButton("Engineering"),
+                buildButton("Biology"),
+                buildButton("English"),
+                buildButton("Math"),
+              ]),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Chemistry"),
+                  buildButton("Physics"),
+                  buildButton("Information Technology")
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Sociology"),
+                  buildButton("Psychology"),
+                  buildButton("Computer Science")
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Hospitality"),
+                  buildButton("Animal Science"),
+                  buildButton("Education")
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Sports",
+                style: GoogleFonts.raleway(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                buildButton("Football"),
+                buildButton("Soccer"),
+                buildButton("Swimming"),
+                buildButton("Baseball")
+              ]),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Basketball"),
+                  buildButton("Esports"),
+                  buildButton("Snow Sports")
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Badminton"),
+                  buildButton("Volleyball"),
+                  buildButton("Tennis"),
+                  buildButton("Rowing")
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Hobbies",
+                style: GoogleFonts.raleway(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Drawing"),
+                  buildButton("Singing"),
+                  buildButton("Gardening"),
+                  buildButton("Gym")
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Fraternity"),
+                  buildButton("Sorority"),
+                  buildButton("Digital Art"),
+                  buildButton("Archery")
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Cooking"),
+                  buildButton("Baking"),
+                  buildButton("Eating"),
+                  buildButton("Gaming")
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Movies"),
+                  buildButton("Studying"),
+                  buildButton("Watching TV"),
+                  buildButton("Anime")
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Languages",
+                style: GoogleFonts.raleway(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Chinese"),
+                  buildButton("German"),
+                  buildButton("Spanish"),
+                  buildButton("Korean")
+                ],
+              ),
+              SizedBox(height: 10),
+              TextButton(
+                  onPressed: () {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
                         builder: (context) => const SearchPage(),
-                      ));
-                },
-                child: Text(
-                  'Skip',
-                  style: GoogleFonts.raleway(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black,
-                  ),
-                ))
-          ]),
-          SizedBox(height: 10),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            buildButton("Cooking"),
-            buildButton("Spanish"),
-            buildButton("English"),
-            buildButton("Running"),
-          ]),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton("Engineering"),
-              buildButton("Mechanical Engineering"),
-              buildButton("Movies")
-            ],
-          ),
-          SizedBox(height: 5),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            buildButton("Aerospace Engineering"),
-            buildButton("Skiing"),
-            buildButton("Dancing")
-          ]),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton("Space"),
-              buildButton("Esports"),
-              buildButton("Billiards"),
-              buildButton("Singing")
-            ],
-          ),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton("Badminton"),
-              buildButton("Basketball"),
-              buildButton("Eating"),
-              buildButton("Soccer")
-            ],
-          ),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton("Football"),
-              buildButton("Agriculture"),
-              buildButton("Art"),
-              buildButton("Internships")
-            ],
-          ),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton("Fraternity"),
-              buildButton("Sorority"),
-              buildButton("Digital Art"),
-              buildButton("Archery")
-            ],
-          ),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton("Computer Science"),
-              buildButton("Information Technology")
-            ],
-          ),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton("Psychology"),
-              buildButton("Japanese"),
-              buildButton("Korean"),
-              buildButton("French")
-            ],
-          ),
-          SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton("Chinese"),
-              buildButton("German"),
-              buildButton("Biology"),
-              buildButton("Math")
-            ],
-          ),
-          SizedBox(height: 5),
-          TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SearchPage(),
-                  ),
-                );
-              },
-              child: Text(
-                "Next",
-                style: GoogleFonts.raleway(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.black,
-                ),
-              ))
-        ] //column children
-            ));
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Next",
+                    style: GoogleFonts.raleway(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                    ),
+                  ))
+            ] //column children
+                )));
   }
 }
 
@@ -169,9 +234,9 @@ class InterestsPage extends StatelessWidget {
 Widget buildButton(String label) {
   return Row(mainAxisAlignment: MainAxisAlignment.end, children: [
     TextButton(
-      child: Text(label, style: TextStyle(fontSize: 14)),
+      child: Text(label, style: TextStyle(fontSize: 15)),
       style: ButtonStyle(
-          padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+          padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(17)),
           foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
               RoundedRectangleBorder(
@@ -182,6 +247,25 @@ Widget buildButton(String label) {
       },
     )
   ]);
+}
+
+Widget buildSearchBar(String label) {
+  return Container(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10.0),
+      color: Colors.grey[200],
+    ),
+    child: const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search...',
+          border: InputBorder.none,
+          icon: Icon(Icons.search),
+        ),
+      ),
+    ),
+  );
 }
 
 Widget buildTextField(String label) {
--- lib/src/screens/searchpage.dart
+++ lib/src/screens/searchpage.dart
@@ -25,36 +25,97 @@ class SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios),
-          color: Colors.black,
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const LoginPage()),
-            );
-          },
+        appBar: AppBar(
+          title: Text("BroncoBond",
+              style: GoogleFonts.raleway(
+                  textStyle: Theme.of(context).textTheme.displaySmall,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w800,
+                  color: const Color(0xFF3B5F43))),
         ),
-        title: Text(
-          "BroncoBond",
-          style: GoogleFonts.raleway(
-            textStyle: Theme.of(context).textTheme.displaySmall,
-            fontSize: 25,
-            fontWeight: FontWeight.w800,
-            color: const Color(0xFF3B5F43),
+        body: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+              Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: buildSearchBar(" ")),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildIcon("Organization", "orgIcon.png"),
+                  buildIcon("Professors", "profIcon.png"),
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildIcon("People", "peopleIcon.png"),
+                  buildIcon("Messages", "messagesIcon.png"),
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildIcon("Events", "eventsIcon.png"),
+                  buildIcon("Forums", "messagesIcon.png"),
+                ],
+              )
+            ])));
+  }
+
+  Widget buildSearchBar(String label) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.grey[200],
+      ),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'Search...',
+            border: InputBorder.none,
+            icon: Icon(Icons.search),
           ),
         ),
-        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(email),
-          ],
-        ),
-      ),
+    );
+  }
+
+  Widget buildIcon(String title, String imagePath) {
+    return Column(
+      children: [
+        Center(
+            child: SizedBox(
+          width: 175,
+          height: 175,
+          child: Card(
+              clipBehavior: Clip.hardEdge,
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () {
+                  debugPrint('${title} tapped.');
+                },
+
+                child: imagePath.isNotEmpty
+                    ? Image(
+                        image: AssetImage('assets/images/$imagePath'),
+                        fit: BoxFit.cover)
+                    : Center(
+                        child:
+                            Text(title)), // Display text if imagePath is empty
+              )),
+        )),
+        SizedBox(height: 5),
+        Center(
+            child: Text(
+          title,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ))
+      ],
     );
   }
 }
