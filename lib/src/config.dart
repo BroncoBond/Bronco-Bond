c74cbb6df46122caef2e27882f8701c6852e7964
Mohraiel <mohraielmatta@gmail.com>
2023-12-07 02:09:55 -0800

    config

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,6 +1,5 @@
-
-const String url = 'http://192.168.1.158:3000/';
+//when running, change to your ip
+const String url = 'http://your-ip:3000/';
 const String registration = '${url}registration';
 const String login = '${url}login';
 const String search = '${url}search';
-
