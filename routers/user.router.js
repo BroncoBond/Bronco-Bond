94f2f807d22d6e9518b03aa7999eb83fb7dfb5c7
Jayden <jayden.vinh.nguyen@gmail.com>
2024-01-05 17:38:31 -0800

    Small Change, route /registration to /register

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const UserController = require("../controller/user.controller");
 
-router.post('/registration', UserController.register);
+router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.get('/search', UserController.searchUserByUsernameOrEmail);
 
