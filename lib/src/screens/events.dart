d7d2ce89dfcc653f9fe9b365e74cb14dd169ef41
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-01-05 18:25:44 -0800

    add placeholder main pages

---
+++ lib/src/screens/events.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/// Displays detailed information about a SampleItem.
+class EventsPage extends StatefulWidget {
+  const EventsPage({super.key});
+
+  @override
+  EventsPageState createState() => EventsPageState();
+}
+
+class EventsPageState extends State<EventsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          color: Colors.black,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text(
+          "BroncoBond",
+          style: GoogleFonts.raleway(
+            textStyle: Theme.of(context).textTheme.displaySmall,
+            fontSize: 25,
+            fontWeight: FontWeight.w800,
+            color: const Color(0xFF3B5F43),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'Events Page',
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          color: Colors.black,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text(
+          "BroncoBond",
+          style: GoogleFonts.raleway(
+            textStyle: Theme.of(context).textTheme.displaySmall,
+            fontSize: 25,
+            fontWeight: FontWeight.w800,
+            color: const Color(0xFF3B5F43),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'Home Page',
+        ),
+      ),
+    );
+  }
+}
