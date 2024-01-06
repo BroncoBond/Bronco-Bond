b0f8b40bf03635956bfa8e736da762362a932a3a
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-01-05 20:02:02 -0800

    added variables

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,5 +1,7 @@
 //when running, change to your ip
 const String url = 'http://your-ip:3000/';
-const String registration = '${url}registration';
-const String login = '${url}login';
-const String search = '${url}search';
+const String registration = '${url}/user/registration';
+const String login = '${url}/user/login';
+const String search = '${url}/user/search';
+const String updateUser = '${url}/user/:id'; // replace :id with user's ID
+const String deleteUser = '${url}/user/:id';
