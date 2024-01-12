05a5d7ee233865ebb4bb8665de8fcc62069adec2
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-01-11 22:51:59 -0800

    added new route

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,7 +1,9 @@
 //when running, change to your ip
-const String url = 'http://192.168.1.9:3000';
+const String url = 'http://your-ip:3000';
 const String register = '${url}/user/register';
 const String login = '${url}/user/login';
 const String search = '${url}/user/search';
-const String updateUser = '${url}/user/:id'; // replace :id with user's ID
-const String deleteUser = '${url}/user/:id';
+const String updateUser =
+    '${url}/user/userUpdate'; // replace :id with user's ID
+const String deleteUser = '${url}/user';
+const String getUserByID = '${url}/user';
