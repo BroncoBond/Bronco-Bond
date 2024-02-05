3251f8cd02c965d8131e0b63c18e0706b5077a93
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-02-04 19:36:16 -0800

    config with more routes

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -7,3 +7,6 @@ const String updateUser =
     '${url}/user/userUpdate'; // replace :id with user's ID
 const String deleteUser = '${url}/user';
 const String getUserByID = '${url}/user';
+const String bondUser = '${url}/user/bond';
+const String unbondUser = '${url}/user/unbond';
+const String logout = '${url}/user/logout';
