69492b92e8d8b492a011becf7541faa32952358f
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2024-02-10 22:15:55 -0800

    Fixed ios folder

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jaydennguyen/Library/Mobile Documents/com~apple~CloudDocs/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jaydennguyen/Desktop/CPP Bronco Bond/Bronco-Bond"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/jaydennguyen/Desktop/CPP Bronco Bond/Bronco-Bond/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -9,3 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/jaydennguyen/Desktop/CPP Bronco Bond/Bronco-Bond/.dart_tool/package_config.json"
