01abb34092a1cbde37a5a427da226c8ed53ba382
mleilani <mleilanifelipe@gmail.com>
2024-03-04 01:37:23 -0800

    added edit profile button

---
+++ lib/src/screens/edit_profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/// Displays detailed information about a SampleItem.
+class EditProfile extends StatefulWidget {
+  const EditProfile({super.key});
+
+  @override
+   EditProfilePageState createState() =>  EditProfilePageState();
+}
+
+class EditProfilePageState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
