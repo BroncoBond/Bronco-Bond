1eae194a245fed771ecb9078c4c989f97361805e
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-03-10 05:17:18 -0700

    simple chat ui

--- lib/src/screens/chat_page.dart
+++ lib/src/screens/chat_page.dart
@@ -9,6 +9,8 @@ class ChatPage extends StatefulWidget {
 }
 
 class ChatPageState extends State<ChatPage> {
+  bool isOutgoingMessage = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,10 +27,94 @@ class ChatPageState extends State<ChatPage> {
         backgroundColor: const Color(0xFF3B5F43),
         automaticallyImplyLeading: false,
       ),
-      body: const Center(
-        child: Text(
-          'Chat Page',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  bool isOutgoing = index % 2 == 0;
+                  return MessageBubble(
+                    isOutgoing: isOutgoing,
+                    message: 'Message $index',
+                  );
+                }),
+          ),
+          buildMessageField()
+        ],
+      ),
+    );
+  }
+
+  Widget buildMessageField() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: 'Message...',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(color: Color(0xffABABAB)),
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: const BorderSide(color: Color(0xFF3B5F43))),
+          suffixIcon: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.send_rounded, color: Color(0xff3B5F43)),
+          ),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
         ),
+      ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  final bool isOutgoing;
+  final String message;
+
+  const MessageBubble({
+    required this.isOutgoing,
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: FractionallySizedBox(
+              widthFactor: 0.6,
+              alignment:
+                  isOutgoing ? Alignment.centerRight : Alignment.centerLeft,
+              child: Container(
+                padding: const EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  color: isOutgoing
+                      ? const Color(0xFF3B5F43)
+                      : const Color(0xFFD9D9D9),
+                  borderRadius: BorderRadius.circular(8.0),
+                  border: Border.all(color: const Color(0xFFABABAB)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      message,
+                      style: TextStyle(
+                          color: isOutgoing ? Colors.white : Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
