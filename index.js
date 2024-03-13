fa7b9dcb58ae26a568712abe662fcaa6a290407d
Jayden Nguyen <Jayden.vinh.nguyen@gmail.com>
2024-03-13 00:50:45 -0700

    SocketIO [Incomplete]

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // External dependencies
 const express = require('express');
-const helmet = require("helmet");
-const morgan = require("morgan");
-var http = require("http");
-const cors = require("cors");
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const http = require('http');
 
 // Internal dependencies
 const userRouter = require('./routers/user.router');
@@ -12,12 +12,10 @@ const requestDurationLogger = require('./middleware/durationLogger');
 
 const app = express();
 const port = process.env.WEBSITES_PORT || process.env.PORT;
-var server = http.createServer(app);
-var io = require("socket.io")(server,{
-    cors: {
-        origin:"*"
-    }
-});
+
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 // Middleware
 app.use(express.json({ limit: process.env.JSON_LIMIT || '50mb' }));
@@ -25,11 +23,6 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use(express.static('public'));
 app.use(cors());
-
-//io socket
-io.on("Connection",(socket)=>{
-    console.log("connected");
-});
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
@@ -43,7 +36,8 @@ app.use(errorHandler);
 
 // Root route
 app.get('/', (req, res) => {
-    res.render('index', { title: 'BroncoBond', message: 'Hello World, this is BroncoBond!' });
+    //res.render('index', { title: 'BroncoBond', message: 'Hello World, this is BroncoBond!' });
+    res.sendFile(__dirname + '/index.html');
 });
 
 // Start the server
@@ -52,6 +46,7 @@ startServer(port);
 function startServer(port) {
     const server = app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
+        startSocketIO(server);
     });
 
     server.on('error', (error) => {
@@ -64,6 +59,22 @@ function startServer(port) {
         server.close(() => {
             console.log('Server is shutting down');
             process.exit(0);
+        });
+    });
+}
+
+function startSocketIO(server) {
+    const io = require('socket.io')(server, {
+        cors: {
+            origin: "*"
+        }
+    });
+
+    console.log("startSocketIo:");
+    io.on('connection', (socket) => {
+        console.log('a user connected');
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
         });
     });
 }
