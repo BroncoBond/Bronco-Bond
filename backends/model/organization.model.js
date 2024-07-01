02eea1e61bb3b6191ed372a57927948b163369d1
Brandon Tseng <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-06-30 21:32:30 -0700

    Organization model and database schema

---
+++ backends/model/organization.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const db = require('../config/db');
+
+const { Schema } = mongoose;
+
+const organizationSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    logo: {
+      data: Buffer,
+      contentType: String,
+    },
+    description: {
+      type: String,
+      max: 300,
+      default: 'No description.',
+    },
+    tokens: [{ type: Object }],
+  },
+  { timestamps: true }
+);
+
+const Organization = db.model('Organization', organizationSchema);
+
+module.exports = Organization;
