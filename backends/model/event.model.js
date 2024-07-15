71f0fd294d43e7e9c3b9e064b9f72d6a77d03eb4
Jayden <Jayden.vinh.nguyen@gmail.com>
2024-07-14 22:25:47 -0700

    Added event model

---
+++ backends/model/event.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const db = require('../config/db');
+
+const { Schema } = mongoose;
+
+const eventSchema = new Schema(
+  {
+    title: {
+      type: String,
+      min: 1,
+      max: 20,
+      required: true,
+    },
+    image: {
+      data: Buffer,
+      contentType: String,
+    },
+    description: {
+      type: String,
+      max: 300,
+      default: 'No description.',
+    },
+  },
+  { timestamps: true }
+);
+
+const event = db.model('Events', eventSchema);
+
+module.exports = event;
