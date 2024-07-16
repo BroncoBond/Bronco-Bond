12c37947f13d2035fa5e6f9df83820b958c243e9
Brandon Tseng <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-07-16 14:27:37 -0700

    Updated Professor schema to validate college and department inputs

--- backends/model/professor.model.js
+++ backends/model/professor.model.js
@@ -2,6 +2,71 @@ const mongoose = require('mongoose');
 const db = require('../config/db');
 const { Schema } = mongoose;
 
+const collegesDepartments = {
+  'Agriculture': [
+    'Agribusiness & Food Industry Management/Agricultural Science',
+    'Animal & Veterinary Sciences',
+    'Apparel Merchandising & Management',
+    'Nutrition & Food Science',
+    'Plant Science',
+  ],
+  'Business Administration': [
+    'Accounting',
+    'Computer Information Systems',
+    'Finance, Real Estate, & Law',
+    'International Business & Marketing',
+    'Management & Human Resources',
+    'Technology & Operations Management',
+  ],
+  'Education and Integrative Studies': [
+    'Early Childhood Studies',
+    'Education',
+    'Interdisciplinary General Education',
+    'Liberal Studies',
+    'Educational Leadership',
+  ],
+  'Engineering': [
+    'Aerospace Engineering',
+    'Chemicals & Materials Engineering',
+    'Civil Engineering',
+    'Electrical & Computer Engineering',
+    'Industrial & Manufacturing Engineering',
+    'Mechanical Engineering',
+  ],
+  'Environmental Design': [
+    'Architecture',
+    'Art',
+    'John T. Lyle Center for Regenerative Studies',
+    'Landscape Architecture',
+    'Urban & Regional Planning',
+  ],
+  'Hospitality Management': [],
+  'Letters, Arts, and Social Sciences': [
+    'Communication',
+    'Economics',
+    'English & Modern Languages',
+    "Ethnic and Women's Studies",
+    'Geography & Anthropology',
+    'History',
+    'Music',
+    'Philosophy',
+    'Political Science',
+    'Psychology',
+    'Sociology',
+    'Theatre & New Dance',
+  ],
+  'Science': [
+    'Biological Sciences',
+    'Chemistry and Biochemistry',
+    'Computer Science',
+    'Geological Sciences',
+    'Kinesiology & Health Promotion',
+    'Mathematics & Statistics',
+    'Physics & Astronomy',
+  ],
+  'Professional and Global Education': [],
+};
+
 const professorSchema = new Schema(
   {
     email: {
@@ -26,103 +91,26 @@ const professorSchema = new Schema(
     },
     college: {
       type: String,
-      enum: [
-        'Agriculture',
-        'Business Administration',
-        'Education and Integrative Studies',
-        'Engineering',
-        'Environmental Design',
-        'Hospitality Management',
-        'Letters, Arts, and Social Sciences',
-        'Science',
-        'Professional and Global Education',
-      ],
       required: true,
+      enum: Object.keys(collegesDepartments),
     },
     department: {
       type: String,
-      enum: function () {
-        if (this.college === 'Don B. Huntley College of Agriculture') {
-          return [
-            'Agribusiness & Food Industry Management/Agricultural Science',
-            'Animal & Veterinary Sciences',
-            'Apparel Merchandising & Management',
-            'Nutrition & Food Science',
-            'Plant Science',
-          ];
-        } else if (this.college === 'College of Business Administration') {
-          return [
-            'Accounting',
-            'Computer Information Systems',
-            'Finance, Real Estate, & Law',
-            'International Business & Marketing',
-            'Management & Human Resources',
-            'Technology & Operations Management',
-          ];
-        } else if (
-          this.college === 'College of Education and Integrative Studies'
-        ) {
-          return [
-            'Early Childhood Studies',
-            'Education',
-            'Interdisciplinary General Education',
-            'Liberal Studies',
-            'Educational Leadership',
-          ];
-        } else if (this.college === 'College of Engineering') {
-          return [
-            'Aerospace Engineering',
-            'Chemicals & Materials Engineering',
-            'Civil Engineering',
-            'Electrical & Computer Engineering',
-            'Industrial & Manufacturing Engineering',
-            'Mechanical Engineering',
-          ];
-        } else if (this.college === 'College of Environmental Design') {
-          return [
-            'Architecture',
-            'Art',
-            'John T. Lyle Center for Regenerative Studies',
-            'Landscape Architecture',
-            'Urban & Regional Planning',
-          ];
-        } else if (
-          this.college === 'College of Letters, Arts, and Social Sciences'
-        ) {
-          return [
-            'Communication',
-            'Economics',
-            'English & Modern Languages',
-            "Ethnic and Women's Studies",
-            'Geography & Anthropology',
-            'History',
-            'Music',
-            'Philosophy',
-            'Political Science',
-            'Psychology',
-            'Sociology',
-            'Theatre & New Dance',
-          ];
-        } else if (this.college === 'College of Science') {
-          return [
-            'Biological Sciences',
-            'Chemistry and Biochemistry',
-            'Computer Science',
-            'Geological Sciences',
-            'Kinesiology & Health Promotion',
-            'Mathematics & Statistics',
-            'Physics & Astronomy',
-          ];
-        } else {
-          // College of Professional and Global Education and Collins College of Hospitality Management both have a single department
-          return [];
-        }
+      required: true,
+      validate: {
+        validator: function (value) {
+          return collegesDepartments[this.college]?.includes(value);
+        },
+        message: (props) =>
+          `${props.value} is not a valid department for the college ${props.path.replace(
+            '.department',
+            '.college'
+          )}`,
       },
     },
     classes: {
       type: [String],
       default: [],
-      required: true,
     },
   },
   { timestamps: true }
