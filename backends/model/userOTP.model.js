667198dbe847fb9ca87424614641873d4df6e78c
Brandon Tseng <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-07-22 15:14:06 -0700

    Removed unnecessary import

---
+++ backends/model/userOTP.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const db = require('../config/db');
+const { Schema } = mongoose;
+
+const userOTPSchema = new Schema({
+    userId: {
+        type: String
+    },
+    otp: {
+        type: String
+    },
+    createdAt: {
+        type: Date
+    },
+    expiresAt: {
+        type: Date
+    },
+});
+
+const UserOTP = db.model('UserOTP', userOTPSchema);
+
+module.exports = UserOTP;
