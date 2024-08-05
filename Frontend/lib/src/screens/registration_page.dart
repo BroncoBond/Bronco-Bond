da6b3088d0aeda03e3706eb18daf3fad8ac08be8
Jacob Lembach <59783225+Sweetzzzz@users.noreply.github.com>
2024-08-04 21:03:07 -0700

    Registration page
    Updated registration page, need help with negative padding

--- Frontend/lib/src/screens/registration_page.dart
+++ Frontend/lib/src/screens/registration_page.dart
@@ -86,55 +86,95 @@ class RegisterPageState extends State<RegisterPage> {
     }
   }
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      
       appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_rounded),
-          color: Colors.black,
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-        title: Text(
-          "BroncoBond",
-          style: GoogleFonts.raleway(
-            textStyle: Theme.of(context).textTheme.displaySmall,
-            fontSize: 25,
-            fontWeight: FontWeight.w800,
-            color: const Color(0xFF3B5F43),
-          ),
-        ),
-        centerTitle: true,
+        title: Text('BroncoBond'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            buildTextField("Username*", usernameController),
-            buildTextField("Email*", emailController),
-            buildTextFieldWithToggle(
-                "Password*", passwordController, _obscurePassword, () {
-              setState(() {
-                _obscurePassword = !_obscurePassword;
-              });
-            }),
-            buildTextFieldWithToggle(" Confirm Password*",
-                confirmPasswordController, _obscureConfirmPassword, () {
-              setState(() {
-                _obscureConfirmPassword = !_obscureConfirmPassword;
-              });
-            }),
-            LoginPageState.buildMainButton("Next", context,
-                (BuildContext context) {
-              registerUser(context);
-            }),
-          ],
-        ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff55685A)
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        margin: EdgeInsets.only(top: 1.0, left: 15.0),
+                        height: 12.0,
+                        width: 190.0,
+                        decoration: BoxDecoration(
+                          color: Color(0xffFED154),
+                          borderRadius: BorderRadius.circular(6.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  userTextField("", usernameController),
+                  buildTextField("", emailController),
+                  buildTextFieldWithToggle(
+                    "", passwordController, _obscurePassword, () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    }),
+                  confirmTextFieldWithToggle(
+                    "", confirmPasswordController, _obscureConfirmPassword, () {
+                      setState(() {
+                        _obscureConfirmPassword = !_obscureConfirmPassword;
+                      });
+                    }),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: GestureDetector(
+              onTap: () {
+                // Add your onTap code here!
+              },
+              child: Container(
+                width: 60.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  color: Color(0xff435F49),
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  Icons.arrow_forward,
+                  color: Color(0xffFED154),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+
 
   // Widget for TextFields
   Widget buildTextFieldWithToggle(
@@ -160,7 +200,7 @@ class RegisterPageState extends State<RegisterPage> {
           // Text field
           SizedBox(
             width: 327,
-            height: 43,
+            height: 48,
             child: Stack(
               alignment: Alignment.centerRight,
               children: [
@@ -168,17 +208,18 @@ class RegisterPageState extends State<RegisterPage> {
                   controller: fieldController,
                   keyboardType: TextInputType.text,
                   obscureText: obscureText,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Color(0xFFABABAB)),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Color(0xFF3B5F43)),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 12),
-                  ),
+                   decoration: InputDecoration(
+                labelText: "Password",
+                labelStyle: TextStyle(color: Color(0xff939393), fontSize: 18) ,
+                filled: true,
+                fillColor:Color(0xffDDDDDD),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(11),
+                ),
+               
+                contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+              ),
                   textAlign: TextAlign.start,
                 ),
                 IconButton(
@@ -195,6 +236,79 @@ class RegisterPageState extends State<RegisterPage> {
     );
   }
 
+
+
+Widget confirmTextFieldWithToggle(
+      String label,
+      TextEditingController fieldController,
+      bool obscureText,
+      VoidCallback toggleVisibility) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Text label
+          Text(
+             label,
+            style: GoogleFonts.raleway(
+              fontSize: 16,
+              fontWeight: FontWeight.w800,
+              color: Color(0xff939393),
+            ),
+            textAlign: TextAlign.start,
+          ),
+          // Text field
+          SizedBox(
+            width: 327,
+            height: 48,
+            child: Stack(
+              alignment: Alignment.centerRight,
+              children: [
+                TextField(
+                  controller: fieldController,
+                  keyboardType: TextInputType.text,
+                  obscureText: obscureText,
+                   decoration: InputDecoration(
+                labelText: "Confirm Password",
+                labelStyle: TextStyle(color: Color(0xff939393), fontSize: 18) ,
+                filled: true,
+                fillColor:Color(0xffDDDDDD),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(11),
+                ),
+               
+                contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+              ),
+                  textAlign: TextAlign.start,
+                ),
+                IconButton(
+                    onPressed: toggleVisibility,
+                    icon: Icon(
+                      obscureText ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.grey,
+                    ))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
   Widget buildTextField(String label, TextEditingController fieldController) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -207,26 +321,27 @@ class RegisterPageState extends State<RegisterPage> {
             style: GoogleFonts.raleway(
               fontSize: 16,
               fontWeight: FontWeight.w800,
-              color: Colors.black,
+              color: Color(0xff939393),
             ),
             textAlign: TextAlign.start,
           ),
           // Text field
           SizedBox(
             width: 327,
-            height: 43,
+            height: 48,
             child: TextField(
               controller: fieldController,
               keyboardType: TextInputType.text,
               decoration: InputDecoration(
+                labelText: "Email",
+                labelStyle: TextStyle(color: Color(0xff939393), fontSize: 18) ,
+                filled: true,
+                fillColor:Color(0xffDDDDDD),
                 border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Color(0xFFABABAB)),
-                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(11),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Color(0xFF3B5F43)),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
+               
                 contentPadding: const EdgeInsets.symmetric(horizontal: 12),
               ),
               textAlign: TextAlign.start,
@@ -236,6 +351,64 @@ class RegisterPageState extends State<RegisterPage> {
       ),
     );
   }
+
+Widget userTextField(String label, TextEditingController fieldController) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Text label
+          Text(
+            label,
+            style: GoogleFonts.raleway(
+              fontSize: 16,
+              fontWeight: FontWeight.w800,
+              color: Color(0xff939393),
+            ),
+            textAlign: TextAlign.start,
+          ),
+          // Text field
+          SizedBox(
+            width: 327,
+            height: 48,
+            child: TextField(
+              controller: fieldController,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: "Username",
+                labelStyle: TextStyle(color: Color(0xff939393), fontSize: 18) ,
+                filled: true,
+                fillColor:Color(0xffDDDDDD),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(11),
+                ),
+               
+                contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   /*Widget buildButton(String label, BuildContext context) {
     return Align(
