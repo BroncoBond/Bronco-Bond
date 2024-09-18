7fe1ff1259e3d2ae1b3ca9b5b54e0aa413eb5ed6
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-09-17 22:02:03 -0700

    set up page for pin code plugin

--- Frontend/lib/src/screens/verification_page.dart
+++ Frontend/lib/src/screens/verification_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:bronco_bond/src/screens/welcome_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 /// Displays detailed information about a SampleItem.
 class VerificationPage extends StatefulWidget {
@@ -12,6 +16,25 @@ class VerificationPage extends StatefulWidget {
 }
 
 class VerificationPageState extends State<VerificationPage> {
+  TextEditingController textEditingController = TextEditingController();
+  bool hasError = false;
+  String currentText = "";
+
+  final formKey = GlobalKey<FormState>();
+  StreamController<ErrorAnimationType>? errorController;
+
+  @override
+  void initState() {
+    errorController = StreamController<ErrorAnimationType>();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    errorController!.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,32 +47,58 @@ class VerificationPageState extends State<VerificationPage> {
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(left: 50.0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff55685A)),
-                      ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          child: Container(
+                            height: 10.0,
+                            width: 110.0,
+                            decoration: BoxDecoration(
+                              color: Color(0xffFED154),
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Verify',
+                            style: GoogleFonts.raleway(
+                                fontSize: 40,
+                                fontWeight: FontWeight.w800,
+                                color: Color(0xff55685A)),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(left: 30.0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Container(
-                        margin: EdgeInsets.only(top: 1.0, left: 15.0),
-                        height: 12.0,
-                        width: 190.0,
-                        decoration: BoxDecoration(
-                          color: Color(0xffFED154),
-                          borderRadius: BorderRadius.circular(6.0),
+                    padding: const EdgeInsets.symmetric(vertical: 30.0),
+                    child: RichText(
+                      text: TextSpan(
+                        text: "Enter the code sent to ",
+                        children: [
+                          TextSpan(
+                            text: "${widget.email}",
+                            style: GoogleFonts.raleway(
+                              color: Color(0xFF2E4233),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                        style: GoogleFonts.raleway(
+                          color: Color(0xFF2E4233),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
                         ),
                       ),
+                      textAlign: TextAlign.center,
                     ),
                   ),
+                  SizedBox(height: 20.0),
                 ],
               ),
             ),
@@ -59,7 +108,7 @@ class VerificationPageState extends State<VerificationPage> {
             right: 20,
             child: GestureDetector(
               onTap: () {
-                //
+                // TO DO
               },
               child: Container(
                 width: 60.0,
@@ -69,8 +118,9 @@ class VerificationPageState extends State<VerificationPage> {
                   shape: BoxShape.circle,
                 ),
                 child: Icon(
-                  Icons.arrow_forward,
+                  Icons.arrow_forward_rounded,
                   color: Color(0xffFED154),
+                  size: 35.0,
                 ),
               ),
             ),
