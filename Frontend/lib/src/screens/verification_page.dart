eb761fb9ffe08bf76d75bf604cafceab9cf79533
Vivian Trieu <vivian.dtrieu@gmail.com>
2024-09-17 19:57:35 -0700

    set up verification page

--- Frontend/lib/src/screens/verification_page.dart
+++ Frontend/lib/src/screens/verification_page.dart
@@ -2,31 +2,80 @@ import 'package:bronco_bond/src/screens/welcome_page.dart';
 import 'package:flutter/material.dart';
 
 /// Displays detailed information about a SampleItem.
-class VerificationPage extends StatelessWidget {
-  const VerificationPage({super.key});
+class VerificationPage extends StatefulWidget {
+  final String email;
 
+  const VerificationPage({Key? key, required this.email}) : super(key: key);
+
+  @override
+  VerificationPageState createState() => VerificationPageState();
+}
+
+class VerificationPageState extends State<VerificationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Verification Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('This is another page.'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SigninPage()),
-                );
-              },
-              child: const Text('Go to Sign-Up Page'),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff55685A)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        margin: EdgeInsets.only(top: 1.0, left: 15.0),
+                        height: 12.0,
+                        width: 190.0,
+                        decoration: BoxDecoration(
+                          color: Color(0xffFED154),
+                          borderRadius: BorderRadius.circular(6.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: GestureDetector(
+              onTap: () {
+                //
+              },
+              child: Container(
+                width: 60.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  color: Color(0xff435F49),
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  Icons.arrow_forward,
+                  color: Color(0xffFED154),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
